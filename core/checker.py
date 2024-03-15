3eec0930b00b80db996c1f643b73bf5f291347f6
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-03-15 13:48:00 +0300

    Update checker.py

--- core/checker.py
+++ core/checker.py
@@ -4,7 +4,6 @@ import aiohttp
 from bip_utils.bip import bip39
 from eth_account import Account
 from eth_account.account import LocalAccount
-from eth_account.messages import encode_defunct
 from loguru import logger
 from web3.auto import w3
 
@@ -12,36 +11,63 @@ from utils import loader, append_file
 
 Account.enable_unaudited_hdwallet_features()
 
+
+class AccountData:
+    def __init__(self,
+                 address: str,
+                 private_key: str | None = None,
+                 mnemonic: str | None = None):
+        self.address: str = address
+        self.private_key: str | None = private_key
+        self.mnemonic: str | None = mnemonic
+
+
 class Checker:
     def __init__(self,
                  account_data: str) -> None:
         if bip39.Bip39MnemonicValidator().IsValid(mnemonic=account_data):
-            self.account: LocalAccount = Account.from_mnemonic(mnemonic=account_data)
+            account: LocalAccount = Account.from_mnemonic(mnemonic=account_data)
+            self.account: AccountData = AccountData(
+                address=account.address,
+                private_key=account.key.hex(),
+                mnemonic=account_data
+            )
 
         else:
-            self.account: LocalAccount = Account.from_key(private_key=account_data)
+            try:
+                account: LocalAccount = Account.from_key(private_key=account_data)
 
-    async def get_proofs(self,
-                         client: aiohttp.ClientSession) -> dict[str, str]:
+                self.account: AccountData = AccountData(
+                    address=account.address,
+                    private_key=account.key.hex(),
+                )
+
+            except ValueError:
+                try:
+                    self.account: AccountData = AccountData(
+                        address=w3.to_checksum_address(value=account_data)
+                    )
+
+                except ValueError:
+                    raise ValueError('Wrong Account Data')
+
+    async def check_eligible(self,
+                             client: aiohttp.ClientSession) -> bool:
         while True:
             response_text: None = None
 
             try:
-                message_hash = w3.keccak(text=f'Claiming Aevo Airdrop {self.account.address}')
-                signature = self.account.sign_message(encode_defunct(message_hash)).signature.hex()
                 r: aiohttp.ClientResponse = await client.get(
-                    url='https://api-ribbon.vercel.app/api/aevo/airdrop-proof',
+                    url='https://api-ribbon.vercel.app/api/aevo/check-eligibility',
                     params={
-                        'address': self.account.address,
-                        'message': f'Claiming Aevo Airdrop {self.account.address}',
-                        'signature': signature
+                        'address': self.account.address
                     }
                 )
 
                 response_text: str = await r.text()
                 response_json: dict = await r.json(content_type=None)
 
-                return response_json['data']['claim']
+                return response_json['airdrop']
 
             except Exception as error:
                 if response_text:
@@ -53,19 +79,21 @@ class Checker:
 
     async def start_checker(self,
                             client: aiohttp.ClientSession) -> None:
-        proof_result: dict[str, str] = await self.get_proofs(client=client)
-        amount: int = int(proof_result['amount'], 16)
+        is_eligible: bool = await self.check_eligible(client=client)
 
-        if amount > 0:
-            logger.success(f'{self.account.address} | {amount}')
+        if is_eligible:
+            logger.success(f'{self.account.address} | Eligible')
 
             async with asyncio.Lock():
                 await append_file(file_path='result/with_tokens.txt',
-                                  file_content=f'{self.account.key.hex()}\n')
+                                  file_content=(f'{self.account.address}' + f" | {self.account.private_key}"
+                                                if self.account.private_key
+                                                else "" + f" | {self.account.mnemonic}"
+                                  if self.account.mnemonic else "") + "\n")
 
             return
 
-        logger.error(f'{self.account.address} | {amount}')
+        logger.error(f'{self.account.address} | Not Eligible')
 
 
 async def start_checker(account_data: str,
